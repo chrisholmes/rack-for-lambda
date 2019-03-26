5f54f9ca24052388b5e9dd100fa84d43dd694e96
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2019-03-26 08:48:12 +0000

    Creating gem 'rack_for_lambda'... MIT License enabled in config       create  rack_for_lambda/Gemfile       create  rack_for_lambda/lib/rack_for_lambda.rb       create  rack_for_lambda/lib/rack_for_lambda/version.rb       create  rack_for_lambda/rack_for_lambda.gemspec       create  rack_for_lambda/Rakefile       create  rack_for_lambda/README.md       create  rack_for_lambda/bin/console       create  rack_for_lambda/bin/setup       create  rack_for_lambda/.gitignore       create  rack_for_lambda/.travis.yml       create  rack_for_lambda/.rspec       create  rack_for_lambda/spec/spec_helper.rb       create  rack_for_lambda/spec/rack_for_lambda_spec.rb       create  rack_for_lambda/LICENSE.txt Initializing git repo in /Users/christopherholmes/gds/rack_for_lambda/rack_for_lambda Gem 'rack_for_lambda' was successfully created. For more information on making a RubyGem visit https://bundler.io/guides/creating_gem.html

---
+++ lib/rack_for_lambda.rb
@@ -0,0 +1,6 @@
+require "rack_for_lambda/version"
+
+module RackForLambda
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rack_for_lambda/version.rb
@@ -0,0 +1,3 @@
+module RackForLambda
+  VERSION = "0.1.0"
+end
